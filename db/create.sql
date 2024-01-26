666a6564bd35512cdffb0b1dfa7931ff9e290c74
Zane112637 <115639302+Zane112637@users.noreply.github.com>
2024-01-26 14:28:38 +0000

    alle eerste 6 klaar

--- db/create.sql
+++ db/create.sql
@@ -20,10 +20,11 @@ CREATE TABLE products (
 -- want different data? check: https://www.mockaroo.com/910b6c20
 --
 
-insert into products (name, description, code, price) values ('Nike SB Dunk Low Club 58 Gulf', '', '816905633-0', 120);
-insert into products (name, description, code, price) values ('Nike Dunk High Aluminium (GS)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius.', '077030122-3', 11);
-insert into products (name, description, code, price) values ('Nike Flight Legacy', 'Pellentesque at nulla. Suspendisse potenti.', '445924201-X', 13.5);
-insert into products (name, description, code, price) values ('Air Jordan 4 Retro White Thunder', 'Duis bibendum, felis sed interdum venenatis, turpis enim blandit mi, in porttitor pede justo eu massa.', '693155505-7', 13.5);
-insert into products (name, description, code, price) values ('Kiss for Corliss, A (Almost a Bride)', 'Nulla ut erat id mauris vulputate elementum. Nullam varius. Nulla facilisi.', '686928463-6', 14);
-insert into products (name, description, code, price) values ('Velvet Goldmine', 'Nam ultrices, libero non mattis pulvinar, nulla pede ullamcorper augue, a suscipit nulla elit ac nulla.', '492662523-7', 14);
+insert into products (name, description, code, price) values ('Nike SB Dunk Low Club 58 Gulf', 'Released in MARCH 2021', '816905633-0', 120);
+insert into products (name, description, code, price) values ('Nike Dunk High Aluminium (GS)', 'First seen in 1985', '077030122-3', 600);
+insert into products (name, description, code, price) values ('Nike Flight Legacy', 'First seen in 1989', '445924201-X', 90);
+insert into products (name, description, code, price) values ('Air Jordan 4 Retro White Thunder', 'COMING SOON', '693155505-7', 0);
+insert into products (name, description, code, price) values ('Air Jordan 1 Retro Alpha', 'Released in 2007', '686928463-6', 140);
+insert into products (name, description, code, price) values ('Nike Kobe 6 Protro Grinch', 'Released in 2010', '492662523-7', 1000);
 
+insert into products (name, description, code, price) values ('Nike Air Max 97', 'Nulla ut 
